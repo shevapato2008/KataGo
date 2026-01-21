bc44501e716c99cc381673daf34f53dfa62c7e07
Frank <shevapato2008@gmail.com>
2026-01-21 15:51:11 +0800

    conductor(refactor): increase stdout buffer limit to 256KB for large analysis payloads

--- python/realtime_api/katago_wrapper.py
+++ python/realtime_api/katago_wrapper.py
@@ -69,7 +69,7 @@ class KataGoWrapper:
             )
             # INCREASE BUFFER LIMIT for stdout to support large analysis payloads
             if self.process.stdout:
-                self.process.stdout._limit = 200 * 1024  # 200KB limit
+                self.process.stdout._limit = 256 * 1024  # 256KB limit
 
             self.running = True
             self.read_task = asyncio.create_task(self._read_loop())
